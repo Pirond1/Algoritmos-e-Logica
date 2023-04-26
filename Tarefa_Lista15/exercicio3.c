f74602cb60f68f6f4d6500002f9d3b73c95cc16e
Gustavo Pirondi <gustavopirondi@gmail.com>
2023-04-25 22:11:26 -0300

    Lista Exercícios
    3 Primeiros Exercicios da Lista 15

---
+++ Tarefa_Lista15/exercicio3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+	char nome[20];
+	char sobrenome[20];
+	printf("Informe seu Nome: ");
+	scanf("%s", nome);
+	printf("Informe seu Sobrenome: ");
+	scanf("%s", sobrenome);
+	printf("Nome Informado: %s, %s", nome, sobrenome);
+}
